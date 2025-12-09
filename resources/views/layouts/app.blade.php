f4f4e568386357b75cbcf86fb49ac3affe590ab0
Dima <webtime10@gmail.com>
2025-12-09 10:29:56 +0200

    Revert "норм версия на 9 число"
    This reverts commit 4cd3526bb08ae7949b9bbdfbc39a351554843d9e.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,13 +3,20 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1">
-  <title>@yield('title', 'Сайт')</title>
-  @vite(['resources/css/app.css', 'resources/js/app.js'])
+  <title>@yield('title', $title ?? 'Сайт')</title>
+  @livewireStyles
 </head>
 <body class="antialiased">
+  <header class="container mx-auto p-4 flex gap-6">
+    <a href="{{ route('home') }}" wire:navigate>Главная</a>
+    <a href="{{ route('blog.index') }}" wire:navigate>Блог</a>
+  </header>
+
   <main class="container mx-auto p-4">
-    @yield('content')
+    {{ $slot }}
   </main>
+
+  @livewireScripts
 </body>
 </html>
 
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
+use App\Livewire\Home; // ← ВАЖНО: импорт компонента
+
+use App\Livewire\BlogIndex;
+use App\Livewire\BlogShow;
 
 // редирект на логин Filament
 Route::get('/login', fn () => redirect()->route('filament.admin.auth.login'))
     ->name('login');
 
 // главная страница
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', Home::class)->name('home');
+// (альтернатива без use: Route::get('/', \App\Livewire\Home::class)->name('home');)
+
+
+Route::get('/blog', BlogIndex::class)->name('blog.index');
+Route::get('/blog/{post}', BlogShow::class)->name('blog.show'); // {post} = id записи Block1
